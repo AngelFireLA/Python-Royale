53163e538934e68109789ae5b62b54a91de4d3f9
AngelFireLA <55795775+AngelFireLA@users.noreply.github.com>
2023-10-05 09:45:34 +0200

    Add files via upload

--- combat.py
+++ combat.py
@@ -7,9 +7,9 @@ import carte
 from joueur import Joueur
 
 
-def update_elixir(joueur, last_time):
+def update_elixir(joueur, last_time, elixir_cooldown):
     current_time = time.time()
-    if current_time - last_time >= 1:
+    if current_time - last_time >= elixir_cooldown:
         joueur.elixir += 1
         last_time = current_time
     return last_time
@@ -21,24 +21,27 @@ class Combat:
         self.arriere_plan = pygame.image.load("images/" + arriere_plan + ".png")
         self.joueur1: Joueur = joueur1
         self.joueur2: Joueur = joueur2
+        self.elixir_cooldown = 2.4
 
     def demarrer_combat(self):
         # Initialize Pygame
         pygame.init()
 
         # Set up the display
-        width, height = 550, 800
+        width, height = 550, 850
         fenetre = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Python Royale")
 
-        self.joueur1.elixir = 0
-        self.joueur2.elixir = 10
+        self.joueur1.elixir = 5
+        self.joueur2.elixir = 5
         self.joueur1.tours.append(carte.Tour("bleu", 275, 650, "roi", 1000, 100, 50, 1, fenetre))
         self.joueur1.tours.append(carte.Tour("bleu", 100, 550, "princesse", 500, 50, 50, 1, fenetre))
         self.joueur1.tours.append(carte.Tour("bleu", 450, 550, "princesse", 500, 50, 50, 1, fenetre))
-        self.joueur2.tours.append(carte.Tour("rouge", 275, 50, "roi", 1000, 100, 50, 1, fenetre))
-        self.joueur2.tours.append(carte.Tour("rouge", 100, 150, "princesse", 500, 50, 50, 1, fenetre))
-        self.joueur2.tours.append(carte.Tour("rouge", 450, 150, "princesse", 500, 50, 50, 1, fenetre))
+        self.joueur2.tours.append(carte.Tour("rouge", 275, 100, "roi", 1000, 100, 50, 1, fenetre))
+        self.joueur2.tours.append(carte.Tour("rouge", 100, 200, "princesse", 500, 50, 50, 1, fenetre))
+        self.joueur2.tours.append(carte.Tour("rouge", 450, 200, "princesse", 500, 50, 50, 1, fenetre))
+
+        #carte vide en y 730
 
         last_time_j1 = time.time()
         last_time_j2 = time.time()
@@ -54,15 +57,15 @@ class Combat:
             fenetre.blit(self.arriere_plan, (0, 0))
             elixir = pygame.image.load(f"images/elixirbar_{self.joueur1.elixir}.png")
             #elixir = pygame.transform.scale_by(elixir, 0.5)
-            fenetre.blit(elixir, (0, 760))
+            fenetre.blit(elixir, (0, 810))
 
             for tour in self.joueur1.tours:
                 tour.draw()
             for tour in self.joueur2.tours:
                 tour.draw()
 
-            last_time_j1 = update_elixir(self.joueur1, last_time_j1)
-            last_time_j2 = update_elixir(self.joueur2, last_time_j2)
+            last_time_j1 = update_elixir(self.joueur1, last_time_j1, self.elixir_cooldown)
+            last_time_j2 = update_elixir(self.joueur2, last_time_j2, self.elixir_cooldown)
             if self.joueur1.elixir > 10:
                 self.joueur1.elixir = 10
             if self.joueur2.elixir > 10:
